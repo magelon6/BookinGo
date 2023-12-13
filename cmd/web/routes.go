a7c5e095114a1a7d48bf0b988325a0910bffbb52
magelon6 <adizovmsu@gmail.com>
2023-12-13 20:54:14 +0300

    nosurf csrf middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,13 +3,22 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/magelon6/bookinGo/pkg/config"
 	"github.com/magelon6/bookinGo/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-  mux := pat.New()
+  // mux := pat.New()
+
+  // mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+  // mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+
+  mux := chi.NewRouter()
+
+  mux.Use(middleware.Recoverer)
+  mux.Use(NoSurf)
 
   mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
   mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
